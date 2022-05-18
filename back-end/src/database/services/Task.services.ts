baf14699b37f2b94c21d5b7a0a1b192f3efe93c2
Victor Veloso <vitovls37@gmail.com>
2022-05-18 18:27:12 -0300

    feat: Implementando Service de Task

---
+++ back-end/src/database/services/Task.services.ts
@@ -0,0 +1,40 @@
+import Service, { ServicesError } from ".";
+import TaskSchema, { Task } from "../interfaces/Task";
+import TaskModel from "../models/Task.model";
+
+export default class TaskService extends Service<Task> {
+  constructor(model = new TaskModel()) {
+    super(model);
+  }
+
+  create = async (data: Task): Promise<Task | null | ServicesError > => {
+    const parsed = TaskSchema.safeParse(data);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(data)
+  }
+
+  findAll = async (): Promise<Task[] | ServicesError> => {
+    return this.model.findAll();
+  }
+
+  findById = async (id: string): Promise<Task | null | ServicesError> => {
+    return this.model.findById(id);
+  }
+
+  update = async (id: string, data: Task): Promise<Task | null | ServicesError> => {
+    const parsed = TaskSchema.safeParse(data);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.update(id, data);
+  }
+
+  delete = async (id: string): Promise<Task | null | ServicesError> => {
+    return this.model.delete(id);
+  }
+    
+}
