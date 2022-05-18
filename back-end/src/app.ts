da4472d0d7b51d40f773ddf28ad9f8f98ad46d9f
Victor Veloso <vitovls37@gmail.com>
2022-05-18 19:18:40 -0300

    refactor: Adicionando method para adicionar rota e usar json

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -10,6 +10,7 @@ export default class App {
 
   constructor(port = Number(process.env.PORT) || 3000) {
     this.app = express();
+    this.app.use(express.json());
     this.port = port;
   }
 
@@ -19,4 +20,9 @@ export default class App {
       console.log(`App listening on the port ${this.port}...`);
     });
   }
+
+  addRouter(router: express.Router) {
+    this.app.use(router);
+  }
+
 }
