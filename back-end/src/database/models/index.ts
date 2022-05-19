f99d7957f1b98b3a09ced48274aa6fef8741190f
Victor Veloso <vitovls37@gmail.com>
2022-05-18 21:19:35 -0300

    feat: Adicionando metodos de filtros e deletar todas as tasks

--- back-end/src/database/models/index.ts
+++ back-end/src/database/models/index.ts
@@ -1,4 +1,4 @@
-import {Model as ModelMongoose, Document} from 'mongoose';
+import { Model as ModelMongoose, Document } from 'mongoose';
 
 interface IModel<T> {
   create(data: T): Promise<T>;
@@ -6,6 +6,10 @@ interface IModel<T> {
   findById(id: string): Promise<T | null>;
   update(id: string, data: T): Promise<T | null>;
   delete(id: string): Promise<T | null>;
+  deleteAll(id: string): Promise<string>;
+  findByCreatedAt(): Promise<T[]>;
+  findByName(): Promise<T[]>;
+  findByStatus(): Promise<T[]>;
 }
 
 export default abstract class Model<T> implements IModel<T> {
@@ -17,7 +21,18 @@ export default abstract class Model<T> implements IModel<T> {
 
   findById = async (id: string): Promise<T | null> => this.model.findById(id);
 
-  update = async (id: string, data: T): Promise<T | null> => this.model.findByIdAndUpdate(id, {...data}, { new: true });
+  update = async (id: string, data: T): Promise<T | null> => this.model.findByIdAndUpdate(id, { ...data }, { new: true });
 
   delete = async (id: string): Promise<T | null> => this.model.findByIdAndDelete(id);
+
+  deleteAll = async (): Promise<string> => {
+    await this.model.deleteMany({});
+    return 'All tasks deleted';
+  };
+
+  findByCreatedAt = async (): Promise<T[]> => this.model.find().sort({ createdAt: 1 });
+
+  findByName = async (): Promise<T[]> => this.model.find().sort({ name: -1 });
+
+  findByStatus = async (): Promise<T[]> => this.model.find().sort({ status: 1 });
 }
