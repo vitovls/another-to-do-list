dd657564e542a6810017b33ec259f7f6edaa8300
Victor Veloso <vitovls37@gmail.com>
2022-05-18 21:22:48 -0300

    feat: Adicionado rotas para filtragem e deletar tarefas

--- back-end/src/api/routes/index.ts
+++ back-end/src/api/routes/index.ts
@@ -13,9 +13,11 @@ class CustomRouter<T> {
     route: string = controller.route,
   ) {
     this.router.get(route, controller.findAll);
+    this.router.get(`${route}/filter`, controller.findByQuery);
     this.router.get(`${route}/:id`, controller.findById);
     this.router.post(route, controller.create);
     this.router.put(`${route}/:id`, controller.update);
+    this.router.delete(`${route}/delete/`, controller.deleteAll);
     this.router.delete(`${route}/:id`, controller.delete);
   }
 }
