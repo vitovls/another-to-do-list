8c5904701227fbd5a915e35a99e4c4f39ded6d12
Victor Veloso <vitovls37@gmail.com>
2022-05-18 23:47:38 -0300

    feat: Implementando estados inciais e necessarios para aplicação

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState({
+    name: '',
+    status: '',
+  });
+
+  const [listTask, setListTask] = useState([]);
+
+  const functionSetterTask = (event) => {
+    const { name, value } = event.target;
+
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
+
   return (
     <main className="App">
       <h1>
         Another To Do List
       </h1>
       <header>
-        <input type="text" placeholder="Nova Tarefa" />
-        <select>
+        <input name="name" onChange={functionSetterTask} type="text" placeholder="Nova Tarefa" />
+        <select name="status" onChange={functionSetterTask}>
           <option value="">Status da Tarefa</option>
           <option value="A Fazer">A Fazer</option>
           <option value="Feito">Feito</option>
