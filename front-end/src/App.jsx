5c6d15d2cdbac39ed1cb1c470f53e0bcbebc2374
Victor Veloso <vitovls37@gmail.com>
2022-05-18 23:43:24 -0300

    feat: Implementando base da pagina

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,11 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <h1>
         Another To Do List
       </h1>
-    </div>
+      <header>
+        <input type="text" placeholder="Nova Tarefa" />
+        <select>
+          <option value="">Status da Tarefa</option>
+          <option value="A Fazer">A Fazer</option>
+          <option value="Feito">Feito</option>
+          <option value="Cancelado">Cancelado</option>
+          <option value="Em Andamento">Em Andamento</option>
+        </select>
+        <button type="button">Adicionar</button>
+      </header>
+    </main>
   );
 }
 
